221a5ff3b7f0b95702d93182a27bd762b9ea7128
prakash <prakash56755@gmail.com>
2024-03-24 17:11:13 +0100

    addition od numbers

--- pythonworld.py
+++ pythonworld.py
@@ -1,2 +1,15 @@
 a = "hello world"
 print(a)
+
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+
+b = 10
+c= 55.6
+e = 45
+f = 54
+d = add_numbers(b,c)
+sh = add_numbers(e,f)
+print("sum d is",d)
+print("sum sh is",sh)
